5e7e1094259290406547d6df6187d4f8576e5aca
kagariet01 <66681962+KagariET01@users.noreply.github.com>
2023-10-29 10:57:05 +0800

    a

--- main.cpp
+++ main.cpp
@@ -1,84 +1,25 @@
-//#include<bits/stdc++.h>
-#include<iostream>
-#include<cstring>
-#include<algorithm>
-#include<cmath>
-#include<string>
-#include<sstream>
-#include<vector>
-#include<queue>
-#include<deque>
-#include<map>
-#include<set>
-#include<cstring>
-#include<iomanip>
-#include<ctime>
-#include<list>
-
+#include<bits/stdc++.h>
 using namespace std;
-#define INT long long int
-#define endl "\n"
-#define read(n) reader<n>()
-#define DBG if(debug)
-#define PII pair<INT,INT>
-bool debug=0;
-bool noTLE=1;
-template<typename tpe>tpe reader(){
-	tpe re;cin>>re;return re;
-}
-
-const INT mxn=1e5;
-INT lca[mxn+1][30];
-INT n;
-vector<INT> tre[mxn];
-INT deep[mxn];
-
-int main(int argc,char** argv){
-	for(int i=0;i<argc;i++){
-		string nwstr=argv[i];
-		if(nwstr=="-Dev"){
-			debug=1;
-			noTLE=0;
-		}else if(nwstr=="-TLE"){
-			noTLE=0;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n+1],s[100001],g[10],f=0;
+		memset(s,0,sizeof(s));
+		memset(g,0,sizeof(g));
+		for(int i=1;i<=n;i++){
+			cin>>a[i];
+			s[a[i]]++;
 		}
-	}
-	DBG{
-		cout<<"Temp by KagariET01"<<endl;
-		cout<<"My Webpage: https://kagariet01.github.io/about"<<endl;
-		cout<<"===DBG mod on==="<<endl;
-		cout<<"Here's your CFG"<<endl;
-		for(int i=0;i<argc;i++){
-			string nwstr=argv[i];
-			cout<<'['<<nwstr<<']'<<endl;
-		}
-		cout<<"===Code start==="<<endl;
-	}
-	if(noTLE && !debug)cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
-
-	function<int(INT)> solve=[](INT casenum){
-		/*Your code here*/
-		return 0;
-	};
-	bool one_case=0;
-	bool ynans=0;
-	bool eof=0;
-	string yes="YES";
-	string no="NO";
-	INT t=(one_case?1:read(int));
-	for(INT i=0;eof || i<t;i++){
-		INT re=solve(i);
-		if(!ynans){
-			if(re==-1)return 0;
-		}else{
-			if(re==1){
-				cout<<yes<<endl;
-			}else if(re==0){
-				cout<<no<<endl;
-			}else{
-				return 0;
+		for(int i=1;i<=100000;i++){
+			if(s[i]>0){
+				f++;
+				g[f]=s[i];
 			}
 		}
+		if(f==1||f<=2&&abs(g[1]-g[2]<=1))cout<<"yes"<<endl;
+		else cout<<"no"<<endl;
 	}
-	return 0;
 }
